9c2dbb24cd9a5298df991b62a703b6fa055c4577
Robson Burns <65874740+Roby109@users.noreply.github.com>
2021-11-26 16:42:52 +0000

    Add files via upload

---
+++ Portfolio/JavaScript/Index.js
@@ -0,0 +1,17 @@
+const navToggle = document.querySelector('.nav-toggle');
+const themeToggle = document.querySelector('.theme-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('theme-open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
